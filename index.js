85b7f92da6caf3e0829e6892a8cf328c03d577da
abhishek <abhishek7gg7@gmail.com>
2019-09-02 16:08:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,20 @@ var db = new store({path: '/tmp/data.json'});
 
 app.get("/*", function(req,res) {
 res.setHeader("content-type","text/html");
+if (req.headers.host.split(".").length==3) {
 axios("https://raw.githubusercontent.com/theabbie/awto/gh-pages/files/"+req.headers.host.split(".")[0]+".html").then(function(x) {
 res.end(x.data);
 }).catch(function(y) {
 res.status(404).end("404");
 })
+}
+else {
+axios("https://serverers.firebaseio.com/custom/"+req.headers.host.split(".")[0]+".json").then(function(x) {
+res.end(x.data);
+}).catch(function(y) {
+res.status(404).end("404");
+})
+}
 })
 
 app.listen(process.env.PORT);
