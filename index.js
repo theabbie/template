15f5f2e2e38ecd926926b5e6e1a2552f5512945e
abhishek <abhishek7gg7@gmail.com>
2019-09-02 12:49:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ app.get("/*", function(req,res) {
 res.setHeader("content-type","text/html");
 axios("https://raw.githubusercontent.com/theabbie/awto/gh-pages/files/"+req.headers.host.split(".")[0]+".html").then(function(x) {
 res.end(x.data);
+}).catch(function(y) {
+res.status(404).end("404");
 })
 });
 
