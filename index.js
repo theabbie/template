00e2b4eec6518c236c533d245ea55b049028953c
abhishek <abhishek7gg7@gmail.com>
2019-09-02 16:11:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var db = new store({path: '/tmp/data.json'});
 app.get("/*", function(req,res) {
 res.setHeader("content-type","text/html");
 if (req.headers.host.split(".").length==3) {
-axios("https://raw.githubusercontent.com/theabbie/awto/gh-pages/files/"+req.headers.host.split(".")[0]+".html").then(function(x) {
+axios("https://raw.githubusercontent.com/theabbie/awto/gh-pages/"+(req.headers.host.split(".")[0]=="ai"?"":"files/")+req.headers.host.split(".")[0]+".html").then(function(x) {
 res.end(x.data);
 }).catch(function(y) {
 res.status(404).end("404");
