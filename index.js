ff23503631fd10bd2a8ee0d7e7503ae116af9145
abhishek <abhishek7gg7@gmail.com>
2019-09-02 12:28:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ var db = new store({path: '/tmp/data.json'});
 app.get("/*", function(req,res) {
 res.setHeader("content-type","text/html");
 res.end("<body background='"+req.headers.host.split(".")[0]+"'></body>")
-})
+});
 
 app.listen(process.env.PORT);
