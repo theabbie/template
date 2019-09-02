a04abd0b0cd171a6c67d6853ab35ac1a77a277bd
abhishek <abhishek7gg7@gmail.com>
2019-09-02 12:22:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var store = require('data-store');
 var db = new store({path: '/tmp/data.json'});
 
 app.get("/*", function(req,res) {
-res.end(req.headers.host)
+res.setHeader("content-type","text/html");
+res.end("<body background='"+req.headers.host.split(".")[0]+"'></body>")
 })
 
 app.listen(process.env.PORT);
