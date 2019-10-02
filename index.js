49af9b51b2fd260fddb3bc2d47a38100448d2202
abhishek <abhishek7gg7@gmail.com>
2019-10-02 15:31:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
 var app = require('express')();
-var store = require('data-store');
-var axios = require('axios');
-var $ = require("cheerio");
-var db = new store({path: '/tmp/data.json'});
 
 app.get("/*", function(req,res) {
-var appp = req.headers.host.split(".")[0];
-if (appp=="hl") {
-res.end("hi")
-}
-else {
-res.status(404).end("404");
-}
+
 })
 
 app.listen(process.env.PORT);
