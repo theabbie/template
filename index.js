7add09e5c0e1dbf4e5858ec19549d5e736931608
abhishek <abhishek7gg7@gmail.com>
2019-10-15 17:40:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,7 @@
 var app = require('express')();
-var imaps = require('imap-simple');
-const simpleParser = require('mailparser').simpleParser;
 
 app.get("/*", function(req,res) {
-var config = {
-    imap: {
-        user: 'abhishek7gg7@gmail.com',
-        password: 'weppkssgcunlrjfp',
-        host: 'imap.gmail.com',
-        port: 993,
-        tls: true,
-        authTimeout: 3000
-    }
-};
 
-imaps.connect(config).then(function (connection) {
-    return connection.openBox('INBOX').then(function () {
-        var searchCriteria = ['1:10'];
-        var fetchOptions = {
-            bodies: ['HEADER', 'TEXT', ''],
-        };
-        return connection.search(searchCriteria, fetchOptions).then(function (messages) {
-            res.json(messages)
-            });
-        });
-    });
 })
 
 app.listen(process.env.PORT);
